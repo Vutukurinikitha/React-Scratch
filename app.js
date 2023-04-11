4f9917d5d7560dba41f53070a7d83fb1188f0619
gsktec <pandusravankumar@gmail.com>
2023-04-11 11:52:54 +0530

    Lesson-1

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const parent = React.createElement(
+    "div",
+    {id : "parent"},
+    React.createElement(
+        "div",
+        {id : "child"},
+        React.createElement("h1",{id: "h1"},"HerbalProducts")
+    )
+);
+const parentArray = React.createElement(
+    "div",
+    {id : "parent"},
+    [React.createElement("div",{id : "child"}),
+    
+    React.createElement("h1",{id: "h1"},"HerbalProducts Array")
+    ]);
+
+//JSX
+const heading = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Lets make Skin and Hair beautiful");
+console.log(parent);      
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parentArray);
